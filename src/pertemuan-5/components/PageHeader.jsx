2d57ef0060874a1aeba1f3310e08c9ad14fd48e5
suciayu28 <suci24si@mahasiswa.pcr.ac.id>
2026-04-14 21:11:08 +0700

    pertemuan5

---
+++ src/pertemuan-5/components/PageHeader.jsx
@@ -0,0 +1,12 @@
+const PageHeader = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+      <p className="text-gray-400 text-sm">
+        Home / Home Detail / Home Very Detail
+      </p>
+    </div>
+  );
+};
+
+export default PageHeader;
